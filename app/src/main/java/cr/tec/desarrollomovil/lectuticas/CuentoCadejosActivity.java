92a26ba9bd2532c754b77abd446482e4403b3f54
Daniel Pacheco <daniellascarez01@gmail.com>
2016-06-11 13:53:49 -0600

    Nuevas actividades e imagenes

---
+++ app/src/main/java/cr/tec/desarrollomovil/lectuticas/CuentoCadejosActivity.java
@@ -0,0 +1,13 @@
+package cr.tec.desarrollomovil.lectuticas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CuentoCadejosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuento_cadejos);
+    }
+}
--- app/src/main/java/cr/tec/desarrollomovil/lectuticas/LeyendasActivity.java
+++ app/src/main/java/cr/tec/desarrollomovil/lectuticas/LeyendasActivity.java
@@ -1,7 +1,9 @@
 package cr.tec.desarrollomovil.lectuticas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LeyendasActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class LeyendasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leyendas);
+
+
+    }
+
+    public void IrAlCadejos(View view){
+        Intent intent = new Intent(this, CuentoCadejosActivity.class);
+        startActivity(intent);
+    }
+
+    public void IrACegua(View view){
+        Intent intent = new Intent(this, CuentoCeguaActivity.class);
+        startActivity(intent);
     }
 }
