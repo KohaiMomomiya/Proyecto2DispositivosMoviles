ae7c2ae5cb2b37c0691aabf3901e34bd6950aafe
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2016-06-12 15:01:59 -0600

    Mecanismo para detectar problemas al iniciar cuentos y actualización de archivo manifest

--- app/src/main/java/cr/tec/desarrollomovil/lectuticas/CuentoCadejosActivity.java
+++ app/src/main/java/cr/tec/desarrollomovil/lectuticas/CuentoCadejosActivity.java
@@ -3,6 +3,7 @@ package cr.tec.desarrollomovil.lectuticas;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,105 +19,114 @@ import java.util.concurrent.ExecutionException;
 
 public class CuentoCadejosActivity extends AppCompatActivity {
 
-    private static String idCuento;
-    private static String nombreCuento;
-    private static Cuento cuentoCegua;
-    private static int contador;
-    private ArrayList<Parrafo> lista;
-    private TextView textParrafo;
-    private int cantParrafos;
+  private static String idCuento;
+  private static String nombreCuento;
+  private static Cuento cuentoCegua;
+  private static int contador;
+  private ArrayList<Parrafo> lista;
+  private TextView textParrafo;
+  private int cantParrafos;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cuento_cadejos);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    try {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_cuento_cadejos);
 
-        final Intent intent = getIntent();
-        idCuento = intent.getStringExtra("Id");
-        nombreCuento = intent.getStringExtra("Nombre");
+      final Intent intent = getIntent();
+      idCuento = intent.getStringExtra("Id");
+      nombreCuento = intent.getStringExtra("Nombre");
 
-        cuentoCegua = new Cuento(nombreCuento,Integer.parseInt(idCuento));
-        getParrafos();
-        contador = 0;
-        textParrafo = (TextView) findViewById(R.id.textoParrafo);
-        lista = cuentoCegua.getLista();
-        cantParrafos = lista.size();
-        textParrafo.setText(lista.get(contador).getTexto());
+      cuentoCegua = new Cuento(nombreCuento, Integer.parseInt(idCuento));
+      getParrafos();
+      contador = 0;
+      textParrafo = (TextView) findViewById(R.id.textoParrafo);
+      lista = cuentoCegua.getLista();
+      cantParrafos = lista.size();
+      textParrafo.setText(lista.get(contador).getTexto());
 
-        Button btnAnterior = (Button) findViewById(R.id.btnAtras);
-        Button btnSiguiente = (Button) findViewById(R.id.btnAdelante);
-        Button btnMenu = (Button) findViewById(R.id.btnMenu);
-        Button btnPreguntas = (Button) findViewById(R.id.btnPreguntas);
+      Button btnAnterior = (Button) findViewById(R.id.btnAtras);
+      Button btnSiguiente = (Button) findViewById(R.id.btnAdelante);
+      Button btnMenu = (Button) findViewById(R.id.btnMenu);
+      Button btnPreguntas = (Button) findViewById(R.id.btnPreguntas);
 
-        btnSiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(contador+1< cantParrafos){
-                    contador++;
-                    textParrafo.setText(lista.get(contador).getTexto());
-                }else if(contador+1 == cantParrafos){
+      btnSiguiente.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          if (contador + 1 < cantParrafos) {
+            contador++;
+            textParrafo.setText(lista.get(contador).getTexto());
+          } else if (contador + 1 == cantParrafos) {
 
-                }
-            }
-        });
-
-        btnAnterior.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(contador > 0){
-                    contador--;
-                    textParrafo.setText(lista.get(contador).getTexto());
-                }else if(contador == 0){
-                    textParrafo.setText(lista.get(contador).getTexto());
-                }else if(contador < 0){
-                    contador = 0;
-                    textParrafo.setText(lista.get(contador).getTexto());
-                }
-            }
-        });
-
-        btnMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                startActivity(new Intent(CuentoCadejosActivity.this,MainActivity.class));
-            }
-        });
-
-        btnPreguntas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentGo = new Intent(CuentoCadejosActivity.this,PreguntasCadejosActivity.class);
-                intentGo.putExtra("idCuento",idCuento);
-                startActivity(intentGo);
-            }
-        });
-    }
-
-    public void getParrafos(){
-        try{
-            String url = "http://lectuticas.esy.es/selectSectionsById.php?id="+idCuento;
-            String resultado = new AccesoBaseDatos(this,url).execute().get();
-            if (!resultado.equals("")) {
-                JSONObject objeto = new JSONObject(resultado);
-                JSONArray lista = objeto.getJSONArray("Parrafos");
-                for(int i=0;i<lista.length();i++){
-                    JSONObject objetoIndividual = lista.getJSONObject(i);
-                    Parrafo parrafo = new Parrafo(Integer.parseInt(objetoIndividual.getString("Id_Parrafo")),
-                            objetoIndividual.getString("Texto"),Integer.parseInt(objetoIndividual.getString("Numero")));
-                    cuentoCegua.agregarParrafo(parrafo);
-                }
-            } else {
-                Toast.makeText(this, "Error al realizar la consulta", Toast.LENGTH_LONG).show();
-            }
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+          }
         }
+      });
+
+      btnAnterior.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+          if (contador > 0) {
+            contador--;
+            textParrafo.setText(lista.get(contador).getTexto());
+          } else if (contador == 0) {
+            textParrafo.setText(lista.get(contador).getTexto());
+          } else if (contador < 0) {
+            contador = 0;
+            textParrafo.setText(lista.get(contador).getTexto());
+          }
+        }
+      });
+
+      btnMenu.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          finish();
+          startActivity(new Intent(CuentoCadejosActivity.this, MainActivity.class));
+        }
+      });
+
+      btnPreguntas.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent intentGo = new Intent(CuentoCadejosActivity.this,
+              PreguntasCadejosActivity.class);
+          intentGo.putExtra("idCuento", idCuento);
+          startActivity(intentGo);
+        }
+      });
+    } catch (NumberFormatException e) {
+      Log.e("Exception", "Id de historia no tiene formato válido. Dato no existe o es inválido.");
+      Toast.makeText
+          (getApplicationContext(), "Hubo un error al iniciar esta pantalla.",
+              Toast.LENGTH_LONG).show();
+      finish();
     }
+  }
+
+  public void getParrafos() {
+    try {
+      String url = "http://lectuticas.esy.es/selectSectionsById.php?id=" + idCuento;
+      String resultado = new AccesoBaseDatos(this, url).execute().get();
+      if (!resultado.equals("")) {
+        JSONObject objeto = new JSONObject(resultado);
+        JSONArray lista = objeto.getJSONArray("Parrafos");
+        for (int i = 0; i < lista.length(); i++) {
+          JSONObject objetoIndividual = lista.getJSONObject(i);
+          Parrafo parrafo = new Parrafo(Integer.parseInt(objetoIndividual.getString("Id_Parrafo")),
+              objetoIndividual.getString("Texto"), Integer.parseInt(objetoIndividual.getString("Numero")));
+          cuentoCegua.agregarParrafo(parrafo);
+        }
+      } else {
+        Toast.makeText(this, "Error al realizar la consulta", Toast.LENGTH_LONG).show();
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
