7fb91d4e1fc867089068dda24aa267f033588fd2
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2016-06-12 23:26:00 -0600

    Layouts de preguntas tienen botones para volver al menú principal. Texto de cuentos y preguntas tiene fondo negro translúcido.

--- app/src/main/java/cr/tec/desarrollomovil/lectuticas/PreguntasCadejosActivity.java
+++ app/src/main/java/cr/tec/desarrollomovil/lectuticas/PreguntasCadejosActivity.java
@@ -2,10 +2,7 @@ package cr.tec.desarrollomovil.lectuticas;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -18,7 +15,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.PropertyResourceBundle;
 import java.util.concurrent.ExecutionException;
 
 public class PreguntasCadejosActivity extends AppCompatActivity {
@@ -142,5 +138,7 @@ public class PreguntasCadejosActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
 
 }
